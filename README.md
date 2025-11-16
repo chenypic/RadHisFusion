ebd5ce9cc12d9f3146157989f6bab37a4a96aafe
iwei <chenwei9320@sdfmu.edu.cn>
2025-11-16 22:58:18 +0800

    test

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # RadHisFusion
 
-test
+The code for the paper "Deep learning radiopathomics signature to predict recurrence risk of hepatocellular carcinoma after hepatectomy" published in Communications Biology.
 
-test
+## The architecture of RadHisFusion
+
+![](imgs/model.jpg)
+
+## Usage
+
+
+python python main_rad_his_fusion.py
+
+## Acknowledgements
+
+Part of codes are reused from the [PORPOISE](https://github.com/mahmoodlab/PORPOISE). Thanks for the codes of PORPOISE.
+
---
+++ utils/loss_func.py
@@ -0,0 +1,217 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+class NLLSurvLoss(nn.Module):
+    """
+    The negative log-likelihood loss function for the discrete time to event model (Zadeh and Schmid, 2020).
+    Code borrowed from https://github.com/mahmoodlab/Patch-GCN/blob/master/utils/utils.py
+    Parameters
+    ----------
+    alpha: float
+        TODO: document
+    eps: float
+        Numerical constant; lower bound to avoid taking logs of tiny numbers.
+    reduction: str
+        Do we sum or average the loss function over the batches. Must be one of ['mean', 'sum']
+    """
+    def __init__(self, alpha=0.0, eps=1e-7, reduction='mean'):
+        super().__init__()
+        self.alpha = alpha
+        self.eps = eps
+        self.reduction = reduction
+
+    def __call__(self, h, y, t, c):
+        """
+        Parameters
+        ----------
+        h: (n_batches, n_classes)
+            The neural network output discrete survival predictions such that hazards = sigmoid(h).
+        y_c: (n_batches, 2) or (n_batches, 3)
+            The true time bin label (first column) and censorship indicator (second column).
+        """
+
+        return nll_loss(h=h, y=y.unsqueeze(dim=1), c=c.unsqueeze(dim=1),
+                        alpha=self.alpha, eps=self.eps,
+                        reduction=self.reduction)
+
+
+# TODO: document better and clean up
+def nll_loss(h, y, c, alpha=0.0, eps=1e-7, reduction='mean'):
+    """
+    The negative log-likelihood loss function for the discrete time to event model (Zadeh and Schmid, 2020).
+    Code borrowed from https://github.com/mahmoodlab/Patch-GCN/blob/master/utils/utils.py
+    Parameters
+    ----------
+    h: (n_batches, n_classes)
+        The neural network output discrete survival predictions such that hazards = sigmoid(h).
+    y: (n_batches, 1)
+        The true time bin index label.
+    c: (n_batches, 1)
+        The censoring status indicator.
+    alpha: float
+        TODO: document
+    eps: float
+        Numerical constant; lower bound to avoid taking logs of tiny numbers.
+    reduction: str
+        Do we sum or average the loss function over the batches. Must be one of ['mean', 'sum']
+    References
+    ----------
+    Zadeh, S.G. and Schmid, M., 2020. Bias in cross-entropy-based training of deep survival networks. IEEE transactions on pattern analysis and machine intelligence.
+    """
+    # print("h shape", h.shape)
+
+    # make sure these are ints
+    y = y.type(torch.int64)
+    c = c.type(torch.int64)
+
+    hazards = torch.sigmoid(h)
+    # print("hazards shape", hazards.shape)
+
+    S = torch.cumprod(1 - hazards, dim=1)
+    # print("S.shape", S.shape, S)
+
+    S_padded = torch.cat([torch.ones_like(c), S], 1)
+    # S(-1) = 0, all patients are alive from (-inf, 0) by definition
+    # after padding, S(0) = S[1], S(1) = S[2], etc, h(0) = h[0]
+    # hazards[y] = hazards(1)
+    # S[1] = S(1)
+    # TODO: document and check
+
+    # print("S_padded.shape", S_padded.shape, S_padded)
+
+
+    # TODO: document/better naming
+    s_prev = torch.gather(S_padded, dim=1, index=y).clamp(min=eps)
+    h_this = torch.gather(hazards, dim=1, index=y).clamp(min=eps)
+    s_this = torch.gather(S_padded, dim=1, index=y+1).clamp(min=eps)
+    # print('s_prev.s_prev', s_prev.shape, s_prev)
+    # print('h_this.shape', h_this.shape, h_this)
+    # print('s_this.shape', s_this.shape, s_this)
+
+    uncensored_loss = -(1 - c) * (torch.log(s_prev) + torch.log(h_this))
+    censored_loss = - c * torch.log(s_this)
+    
+
+    # print('uncensored_loss.shape', uncensored_loss.shape)
+    # print('censored_loss.shape', censored_loss.shape)
+
+    neg_l = censored_loss + uncensored_loss
+    if alpha is not None:
+        loss = (1 - alpha) * neg_l + alpha * uncensored_loss
+
+    if reduction == 'mean':
+        loss = loss.mean()
+    elif reduction == 'sum':
+        loss = loss.sum()
+    else:
+        raise ValueError("Bad input for reduction: {}".format(reduction))
+
+    return loss
+
+
+
+import torch
+import torch.nn as nn
+
+
+class EDiceLoss(nn.Module):
+    """Dice loss tailored to Brats need.
+    """
+
+    def __init__(self, do_sigmoid=True):
+        super(EDiceLoss, self).__init__()
+        self.do_sigmoid = do_sigmoid
+
+    def binary_dice(self, inputs, targets, label_index):
+        smooth = 1.
+        if self.do_sigmoid:
+            inputs = torch.sigmoid(inputs)
+
+        intersection = EDiceLoss.compute_intersection(inputs, targets)
+        dice = (2 * intersection + smooth) / (inputs.pow(2).sum() + targets.pow(2).sum() + smooth)
+        return 1 - dice
+
+    @staticmethod
+    def compute_intersection(inputs, targets):
+        intersection = torch.sum(inputs * targets)
+        return intersection
+
+    def forward(self, inputs, target):
+        dice = 0
+        ce = 0
+        CE_L = torch.nn.BCELoss()
+        for i in range(target.size(1)):
+            dice = dice + self.binary_dice(inputs[:, i, ...], target[:, i, ...], i)
+            ce = ce + CE_L(torch.sigmoid(inputs[:, i, ...]), target[:, i, ...])
+        final_dice = (0.7 * dice + 0.3 * ce) / target.size(1)
+        return final_dice
+
+    def metric(self, inputs, target):
+        dices = []
+        for j in range(target.size(0)):
+            dice = []
+            for i in range(target.size(1)):
+                dice.append(self.binary_dice(inputs[j, i], target[j, i], i, True))
+            dices.append(dice)
+        return dices
+
+
+class EDiceLoss_Val(nn.Module):
+    """Dice loss tailored to Brats need.
+    """
+
+    def __init__(self, do_sigmoid=True):
+        super(EDiceLoss_Val, self).__init__()
+        self.do_sigmoid = do_sigmoid
+        self.labels = ["ET", "TC", "WT"]
+        self.device = "cpu"
+
+    def binary_dice(self, inputs, targets, label_index, metric_mode=False):
+        smooth = 1.
+        if self.do_sigmoid:
+            inputs = torch.sigmoid(inputs)
+
+        if metric_mode:
+            inputs = inputs > 0.5
+            if targets.sum() == 0:
+                print(f"No {self.labels[label_index]} for this patient")
+                if inputs.sum() == 0:
+                    return torch.tensor(1., device="cuda:0")
+                else:
+                    return torch.tensor(0., device="cuda:0")
+            # Threshold the pred
+        intersection = EDiceLoss_Val.compute_intersection(inputs, targets)
+        if metric_mode:
+            dice = (2 * intersection) / ((inputs.sum() + targets.sum()) * 1.0)
+        else:
+            dice = (2 * intersection + smooth) / (inputs.pow(2).sum() + targets.pow(2).sum() + smooth)
+        if metric_mode:
+            return dice
+        return 1 - dice
+
+    @staticmethod
+    def compute_intersection(inputs, targets):
+        intersection = torch.sum(inputs * targets)
+        return intersection
+
+    def forward(self, inputs, target):
+        dice = 0
+        for i in range(target.size(1)):
+            dice = dice + self.binary_dice(inputs[:, i, ...], target[:, i, ...], i)
+        final_dice = dice / target.size(1)
+        return final_dice
+
+    def metric(self, inputs, target):
+        dices = []
+        for j in range(target.size(0)):
+            dice = []
+            for i in range(target.size(1)):
+                dice.append(self.binary_dice(inputs[j, i], target[j, i], i, True))
+            dices.append(dice)
+        return dices
+
+
